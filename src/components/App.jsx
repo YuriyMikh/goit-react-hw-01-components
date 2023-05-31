0bcc0b2b4e7ce9d39e606544f8f6ed5252486c3d
YuriyMikh <pamukkale1717@gmail.com>
2023-06-01 02:28:38 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,15 @@ import transactions from './Transactions/transactions.json';
 
 export const App = () => {
   return (
-    <div>
+    <div
+    style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}>
       <Profile
         username={user.username}
         tag={user.tag}
