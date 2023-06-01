60dfa59a9121cc2c54aa7d08777129d14b8feb46
YuriyMikh <pamukkale1717@gmail.com>
2023-06-01 16:22:10 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
-import TransactionHistory from 'components/Transactions/TransactionHistory';
+import TransactionHistory from 'components/transactions/TransactionHistory';
 
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends';
-import transactions from './Transactions/transactions.json';
+import transactions from './transactions/transactions.json';
 
 export const App = () => {
   return (
